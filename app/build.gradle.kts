b9041c8f416cce1b0e8a61a2771542227a8687b2
selimdawa <selimdawa@gmail.com>
2026-06-09 03:57:33 +0300

    SDK 37 / Android 17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,15 +10,13 @@ plugins {
 android {
     namespace = "com.littleapp.todonote"
     compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
+        version = release(37)
     }
 
     defaultConfig {
         applicationId = "com.littleapp.todonote"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
